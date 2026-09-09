bd34096c5a7c68516cd42508a34693b72b5e60db
Everett <everettjf@live.com>
2026-09-09 01:10:04 -0700

    Update RiftVM to 0.1.7

--- Casks/riftvm.rb
+++ Casks/riftvm.rb
@@ -2,8 +2,8 @@
 # frozen_string_literal: true
 
 cask "riftvm" do
-  version "0.1.6"
-  sha256 "1d8ff714096f5557d496c64153cc7886a8fbde54b83ed5c12db1cc9f47f49595"
+  version "0.1.7"
+  sha256 "1ecdb542ae77d8f62b6ccef7f4d62f5253f60a4d9ba21acea262e0c3db0f26d0"
 
   url "https://github.com/riftvm/riftvm/releases/download/riftvm-v#{version}/RiftVM-#{version}.zip?notarized=1"
   name "RiftVM"
